14f0bd14f0989041e2ab717e825facf929a38f88
MrJDA <LIUJA11@OOCL.COM>
2020-01-08 15:37:38 +0800

    testing project

---
+++ src/main/java/com/oocl/jenkins/JenkinsTestingApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.jenkins;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestingApplication {
+    public static void main(String[] args){
+        SpringApplication.run(JenkinsTestingApplication.class, args);
+    }
+}
